1e6e87a1c0dab70879bff33cfd190f7c6429edda
Mila Malchikova <milamalchikova@MacBook-Pro-Mila.local>
2025-09-26 20:34:00 +0300

    G

--- laba1.c
+++ laba1.c
@@ -1,13 +1,68 @@
-//лаба 1
+// struct Point {
+//     int x;
+//     int y;
+// };Point;
+//
+// typedef struct Geo {
+//     char*name;
+// };
+//
+// #include <stdio.h>
+// #include <stdlib.h>
+//
+// // Определяем структуру Elem
+// typedef struct Elem {
+//     int Val;
+//     struct Elem *Next;  // указатель на следующий элемент
+// } Elem;
+//
+// int main() {
+//     // Создаём два элемента
+//     Elem a = {10, NULL};
+//     Elem b = {20, NULL};
+//
+//     // Связываем их
+//     a.Next = &b;
+//
+//     // Выводим цепочку
+//     printf("%d -> %d\n", a.Val, a.Next->Val);
+//
+//     return 0; // завершение программы
+// }
+
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-
-#include <stdio.h>
 
 
-int main(void) {
-    // код программы
-    return 0;
+typedef struct Elem {
+    int Val;
+    struct Elem *Next;  // указатель на следующий элемент
+} Elem;
+
+void add(int val, Elem*root) {
+    Elem *last=root;
+    Elem *new_v=(Elem*)malloc(sizeof(Elem));
+    new_v->Val=val;
+    new_v->Next=NULL;
+    while(last->Next!=NULL) {
+        last=last->Next;
+    }
+    last->Next=new_v;
+}
+
+int get(int index, Elem *root);{
+    Elem *last=root;
+    for (int i = 0; i <= index;i++) {
+        last = last->Next;
+    }
+    return last->Val;
+}
+main(){
+    Elem * root = (Elem*)malloc(sizeof(Elem));
+    root->Next=NULL;
+    add(5);
+    add(6);
+    printf("Val -> %d\n", get(3, root));
+    get();
 }
